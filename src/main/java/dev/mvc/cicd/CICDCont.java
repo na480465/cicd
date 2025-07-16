9536e2355fbd852e80f318de739e3a0ef70897d0
na480465 <nayung030703@gmail.com>
2025-07-16 10:24:51 +0900

    workflow 정상 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -9,33 +9,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class CICDCont {
-  public CICDCont() {
-    System.out.println("-> CICDCont created.");
-  }
-
   // http://localhost:9091/cicd?sw=-1
   // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
+  // http://localhost:9091/cicd?sw=2
   @GetMapping("/cicd")
-  public String cicd(@RequestParam(name="sw", defaultValue = "1") Integer sw) { // defaultValue = "1" -> 1
+  public String cicd(@RequestParam(name="sw", defaultValue = "1") Integer sw) {
     String msg = "";
     
     if (sw == -1) {
       msg = "/cicd 호출됨, error 로그";
       log.error(msg);
-      
-      try {
-        int div = 5/0;
-      }catch (Exception e) {
-        log.error(e.toString());
-      }
     } else if (sw == 0) {
       msg = "/cicd 호출됨, warn 로그";
       log.warn(msg);
-    }else if (sw == 1) {
+    } else if (sw == 1) {
       msg = "/cicd 호출됨, info 로그";
       log.info(msg);
+    } else if (sw == 2) {
+      msg = "/cicd workflow 테스트";
+      log.info(msg);
     }
+    
     return "<h3>" + msg + "</h3>";
   }
 }
